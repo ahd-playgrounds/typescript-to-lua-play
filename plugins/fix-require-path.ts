4e5b0661067094a82b11b756c90843f282042689
codethread <adamhalldesigns@gmail.com>
2023-08-17 20:03:54 +0100

    test of nested deps

--- plugins/fix-require-path.ts
+++ plugins/fix-require-path.ts
@@ -4,6 +4,14 @@ import * as tstl from "typescript-to-lua";
 const LUA_PREFIX = "learn";
 
 const plugin: tstl.Plugin = {
+  moduleResolution(
+    moduleIdentifier: string,
+    requiringFile: string,
+    options: tstl.CompilerOptions,
+    emitHost: tstl.EmitHost
+  ) {
+    console.log({ moduleIdentifier, options });
+  },
   beforeEmit(
     _program: ts.Program,
     _options: tstl.CompilerOptions,
@@ -11,6 +19,7 @@ const plugin: tstl.Plugin = {
     result: tstl.EmitFile[]
   ) {
     for (const file of result) {
+      // console.log(file);
       // const REQUIRE_PATH_REGEX = /require\("(src.+)"\)/g;
       const REQUIRE_PATH_REGEX = /require\("(.+)"\)/g;
       file.code = file.code.replaceAll(
